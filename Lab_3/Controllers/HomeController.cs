16432af779435609a9aa1fabb7062f63fe0a911a
Egor <stoliarchukk.egor@gmail.com>
2023-10-02 19:48:57 +0300

    Add project files.

---
+++ Lab_3/Controllers/HomeController.cs
@@ -0,0 +1,120 @@
+﻿using Lab_3.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Lab_3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var trains = _context.Trainers.ToList();
+            return View(trains);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("Name,Specialization,ExperianceYears,Number")] Trainer trainer)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(trainer);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(trainer);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var trainer = _context.Trainers.Find(id);
+            if (trainer == null)
+            {
+                return NotFound();
+            }
+
+            return View(trainer);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, [Bind("TrainerId,Name,Specialization,ExperianceYears,Number")] Trainer trainer)
+        {
+            if (id != trainer.TrainerId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Update(trainer);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(trainer);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var trainer = _context.Trainers.Find(id);
+            if (trainer == null)
+            {
+                return NotFound();
+            }
+
+            return View(trainer);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var trainer = _context.Trainers.Find(id);
+            if (trainer == null)
+            {
+                return NotFound();
+            }
+
+            _context.Trainers.Remove(trainer);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Lab_3/Models/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Lab_3.Models
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Trainer> Trainers { get; set; }
+    }
+}
